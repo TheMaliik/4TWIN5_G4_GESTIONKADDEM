925c5401ca11fc1775b7d071a6e6c080c0996242
Guesmi Melek <157908075+TheMaliik@users.noreply.github.com>
2025-04-02 01:03:18 +0000

    Update UniversiteRestController.java

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -15,71 +15,72 @@ import java.util.Set;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/universite")
+@CrossOrigin(origins = "http://localhost:4200") // Allow requests from this origin
 public class UniversiteRestController {
 
-	private static final Logger logger = LogManager.getLogger(UniversiteRestController.class);
+    private static final Logger logger = LogManager.getLogger(UniversiteRestController.class);
 
-	@Autowired
-	IUniversiteService universiteService;
+    @Autowired
+    IUniversiteService universiteService;
 
-	// http://localhost:8089/Kaddem/universite/retrieve-all-universites
-	@GetMapping("/retrieve-all-universites")
-	public List<Universite> getUniversites() {
-		logger.info("Récupération de toutes les universités");
-		List<Universite> listUniversites = universiteService.retrieveAllUniversites();
-		logger.debug("Nombre d'universités récupérées : {}", listUniversites.size());
-		return listUniversites;
-	}
+    // http://localhost:8089/Kaddem/universite/retrieve-all-universites
+    @GetMapping("/retrieve-all-universites")
+    public List<Universite> getUniversites() {
+        logger.info("Récupération de toutes les universités");
+        List<Universite> listUniversites = universiteService.retrieveAllUniversites();
+        logger.debug("Nombre d'universités récupérées : {}", listUniversites.size());
+        return listUniversites;
+    }
 
-	// http://localhost:8089/Kaddem/universite/retrieve-universite/8
-	@GetMapping("/retrieve-universite/{universite-id}")
-	public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
-		logger.info("Récupération de l'université avec ID : {}", universiteId);
-		Universite universite = universiteService.retrieveUniversite(universiteId);
-		if (universite == null) {
-			logger.warn("Aucune université trouvée avec ID : {}", universiteId);
-		}
-		return universite;
-	}
+    // http://localhost:8089/Kaddem/universite/retrieve-universite/8
+    @GetMapping("/retrieve-universite/{universite-id}")
+    public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
+        logger.info("Récupération de l'université avec ID : {}", universiteId);
+        Universite universite = universiteService.retrieveUniversite(universiteId);
+        if (universite == null) {
+            logger.warn("Aucune université trouvée avec ID : {}", universiteId);
+        }
+        return universite;
+    }
 
-	// http://localhost:8089/Kaddem/universite/add-universite
-	@PostMapping("/add-universite")
-	public Universite addUniversite(@RequestBody Universite u) {
-		logger.info("Ajout d'une nouvelle université : {}", u);
-		Universite universite = universiteService.addUniversite(u);
-		logger.debug("Université ajoutée avec succès : {}", universite);
-		return universite;
-	}
+    // http://localhost:8089/Kaddem/universite/add-universite
+    @PostMapping("/add-universite")
+    public Universite addUniversite(@RequestBody Universite u) {
+        logger.info("Ajout d'une nouvelle université : {}", u);
+        Universite universite = universiteService.addUniversite(u);
+        logger.debug("Université ajoutée avec succès : {}", universite);
+        return universite;
+    }
 
-	// http://localhost:8089/Kaddem/universite/remove-universite/1
-	@DeleteMapping("/remove-universite/{universite-id}")
-	public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
-		logger.info("Suppression de l'université avec ID : {}", universiteId);
-		universiteService.deleteUniversite(universiteId);
-		logger.debug("Université supprimée avec succès : {}", universiteId);
-	}
+    // http://localhost:8089/Kaddem/universite/remove-universite/1
+    @DeleteMapping("/remove-universite/{universite-id}")
+    public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
+        logger.info("Suppression de l'université avec ID : {}", universiteId);
+        universiteService.deleteUniversite(universiteId);
+        logger.debug("Université supprimée avec succès : {}", universiteId);
+    }
 
-	// http://localhost:8089/Kaddem/universite/update-universite
-	@PutMapping("/update-universite")
-	public Universite updateUniversite(@RequestBody Universite u) {
-		logger.info("Mise à jour de l'université : {}", u);
-		Universite updatedUniversite = universiteService.updateUniversite(u);
-		logger.debug("Université mise à jour avec succès : {}", updatedUniversite);
-		return updatedUniversite;
-	}
+    // http://localhost:8089/Kaddem/universite/update-universite
+    @PutMapping("/update-universite")
+    public Universite updateUniversite(@RequestBody Universite u) {
+        logger.info("Mise à jour de l'université : {}", u);
+        Universite updatedUniversite = universiteService.updateUniversite(u);
+        logger.debug("Université mise à jour avec succès : {}", updatedUniversite);
+        return updatedUniversite;
+    }
 
-	@PutMapping(value="/affecter-universite-departement/{universiteId}/{departementId}")
-	public void affectertUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId, @PathVariable("departementId") Integer departementId) {
-		logger.info("Affectation de l'université ID : {} au département ID : {}", universiteId, departementId);
-		universiteService.assignUniversiteToDepartement(universiteId, departementId);
-		logger.debug("Affectation réussie : Université ID {} -> Département ID {}", universiteId, departementId);
-	}
+    @PutMapping(value="/affecter-universite-departement/{universiteId}/{departementId}")
+    public void affectertUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId, @PathVariable("departementId") Integer departementId) {
+        logger.info("Affectation de l'université ID : {} au département ID : {}", universiteId, departementId);
+        universiteService.assignUniversiteToDepartement(universiteId, departementId);
+        logger.debug("Affectation réussie : Université ID {} -> Département ID {}", universiteId, departementId);
+    }
 
-	@GetMapping(value = "/listerDepartementsUniversite/{idUniversite}")
-	public Set<Departement> listerDepartementsUniversite(@PathVariable("idUniversite") Integer idUniversite) {
-		logger.info("Récupération des départements pour l'université ID : {}", idUniversite);
-		Set<Departement> departements = universiteService.retrieveDepartementsByUniversite(idUniversite);
-		logger.debug("Nombre de départements récupérés : {}", departements.size());
-		return departements;
-	}
+    @GetMapping(value = "/listerDepartementsUniversite/{idUniversite}")
+    public Set<Departement> listerDepartementsUniversite(@PathVariable("idUniversite") Integer idUniversite) {
+        logger.info("Récupération des départements pour l'université ID : {}", idUniversite);
+        Set<Departement> departements = universiteService.retrieveDepartementsByUniversite(idUniversite);
+        logger.debug("Nombre de départements récupérés : {}", departements.size());
+        return departements;
+    }
 }
