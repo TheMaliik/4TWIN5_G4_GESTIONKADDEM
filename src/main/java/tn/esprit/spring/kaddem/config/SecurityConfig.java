5e2de88ecc14a271950a18cd5c0f026b7717a70f
Guesmi Melek <157908075+TheMaliik@users.noreply.github.com>
2025-04-16 20:11:27 +0000

    Update and rename CorsFilter.java to SecurityConfig.java

---
+++ src/main/java/tn/esprit/spring/kaddem/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package tn.esprit.spring.kaddem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable() // Enable CORS and disable CSRF
+            .authorizeRequests()
+            .anyRequest().permitAll(); // Allow all requests
+    }
+}
