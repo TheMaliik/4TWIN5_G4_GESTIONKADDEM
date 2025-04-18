882ca3ad5eddf3b5b68f7661c06f243ea0a6a918
ahmed benhmida <benhmida.ahmed1@esprit.tn>
2025-04-18 01:34:30 +0100

    resolved SonarQube issues in CorsFilter.java

--- src/main/java/tn/esprit/spring/kaddem/config/CorsFilter.java
+++ src/main/java/tn/esprit/spring/kaddem/config/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements Filter {
 
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        // response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -34,9 +34,11 @@ public class CorsFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) {
+        throw new UnsupportedOperationException("CORS filter not yet implemented");
     }
 
     @Override
     public void destroy() {
+        throw new UnsupportedOperationException("CORS filter not yet implemented");
     }
 }
