ff649bdb93996ecd8bd6f16018cfaa426822bd66
ahmed benhmida <benhmida.ahmed1@esprit.tn>
2025-04-18 01:43:39 +0100

    resolved SonarQube issues in WebConfig.java

--- src/main/java/tn/esprit/spring/kaddem/config/WebConfig.java
+++ src/main/java/tn/esprit/spring/kaddem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins - in production, specify your Angular app's URL
+                .allowedOrigins("http://localhost:4200") // Allow all origins "*"
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);  // Max age of the pre-flight request in seconds
